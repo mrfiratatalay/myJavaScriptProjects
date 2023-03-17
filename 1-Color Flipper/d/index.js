54e0b66ac8c768e089ab775586c529f03cdab253
FIRAT ATALAY <atafiratbey@gmail.com>
2023-03-17 23:10:05 +0300

    I have been added d.

--- 1-Color Flipper/d/index.js
+++ 1-Color Flipper/d/index.js
@@ -1,40 +1,52 @@
 window.onload = function () {
-  // HTML'de body, getNewColorBtn ve colorBox öğelerini tanımla
-  const body = document.querySelector("body");
-  const getNewColorBtn = document.getElementById("getNewColorBtn");
-  const colorBox = document.getElementById("colorBox");
+  const getColor = () => {
+    // 0 ile 16777215 arasında rastgele bir sayı oluşturulur
+    const randomNumber = Math.floor(Math.random() * 16777215);
 
-  // HEX renk kodlarını oluşturmak için kullanılacak karakterler
-  const hexChars = "0123456789ABCDEF";
+    // Oluşturulan sayı, hex renk koduna çevrilmek üzere toString() metodu kullanılarak 16'lık sayı sistemi kullanılarak string'e dönüştürülür
+    const randomCode = "#" + randomNumber.toString(16);
 
-  // getNewColorBtn öğesine tıklanınca çalışacak işlevi tanımla
-  getNewColorBtn.addEventListener("click", () => {
-    // HEX renk kodunu oluşturmak için başlangıç değeri "#"
-    let hexColorCode = "#";
+    // Sayfanın arka plan rengi, oluşturulan randomCode ile değiştirilir
+    document.body.style.backgroundColor = randomCode;
 
-    // HEX renk kodu altı karakter içerir, bu nedenle altı kez döngü yap
-    for (let i = 0; i < 6; i++) {
-      // hexChars dizisinden rastgele bir karakter indeksi seç
-      let randomNumber = Math.floor(Math.random() * hexChars.length);
+    // Renk kodu, kullanıcının görebilmesi için colorcode id'li öğeye yazdırılır
+    document.getElementById("colorcode").innerText = randomCode;
+  };
 
-      // seçilen indeksteki karakteri HEX renk koduna ekle
-      hexColorCode += hexChars.charAt(randomNumber);
-    }
-
-    // body öğesinin arka plan rengini oluşturulan HEX renk koduna ayarla
-    body.style.backgroundColor = hexColorCode;
-
-    // colorBox öğesine oluşturulan HEX renk kodunu yaz
-    colorBox.innerHTML = `Background Color : <span>${hexColorCode}</span>`;
-  });
+  // getColor fonksiyonu, click olayı gerçekleştiğinde tetiklenir
+  document.getElementById("btn").addEventListener("click", getColor);
 };
 
-//-------------------------------------------------------------------------------
-//              charAt() Method
-/*---------------------------------------------------------------------------------
-charAt() metodu, bir dizideki belirtilen indeksteki karakteri döndürür. Dizideki karakterler sıfırdan başlayarak sayılır. Yani, bir dizi içindeki ilk karakterin indeksi 0'dır, ikincisi 1, üçüncüsü 2, vb.
+//
+//-------------------------------------------------------
+//               toString() METHOD'um
+//--------------------------------------------------------
+/*
+JavaScript'te, bir sayı veya bir nesne için string bir ifade elde etmek için toString() methodu kullanılır. Bu method sayısal ifadeleri string'e dönüştürür ve geri döndürür.
 
-Örneğin, "hello".charAt(0) ifadesi "h" karakterini döndürür. "hello".charAt(1) ifadesi "e" karakterini döndürür.
+Örneğin:
 
-Bu örnekte charAt() metodu, hexChars adlı bir dizideki belirtilen rastgele indeksteki karakteri seçmek için kullanılır ve bu karakter hexColorCode adlı değişkene eklenir.
+javascript
+Copy code
+let num = 123;
+let str = num.toString();
+console.log(str); // "123"
+
+
+Burada toString() methodu, num değişkeninde saklanan sayıyı string bir ifadeye dönüştürür ve str değişkenine atar.
+
+Ayrıca, toString() methodu farklı sayı tabanlarına (binary, octal, hex) dönüştürmek için de kullanılabilir. Bunun için, methoda sayı tabanını belirten bir argüman vermek gerekir. Örneğin:
+
+javascript
+Copy code
+let num = 255;
+let binary = num.toString(2);
+let octal = num.toString(8);
+let hex = num.toString(16);
+console.log(binary); // "11111111"
+console.log(octal); // "377"
+console.log(hex); // "ff"
+
+
+Burada, toString() methodu sırasıyla binary, octal ve hex tabanlarına göre dönüşüm yapar ve sonuçları ilgili değişkenlere atar.
 */
